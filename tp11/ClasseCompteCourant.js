cef05f75e0f0845bfa1608308541fce6a1819d1d
AnanasPTC <lucas.huchede@gmail.com>
2022-09-28 13:36:47 +0200

    tp11 v2

--- tp11/ClasseCompteCourant.js
+++ tp11/ClasseCompteCourant.js
@@ -1,19 +1,37 @@
+function testCompteCourant()
+{
+    odin= new CompteCourant();
+}
+
 class CompteCourant
 {
     constructor(NC,solde)
     {
-
+        this.NC=NC;
+        this.solde=solde;
     }
     infos()
     {
-
+        let para = document.createElement('p');
+        document.getElementById("ares").append(para);
+        para.innerHTML = "compte courant numéro:"+this.NC+",solde="+this.solde+" euro";
     }
-    crediter()
+    crediter(montant)
     {
-
+        this.solde=this.solde+montant;
     }
-    debiter()
+    debiter(montant)
     {
-
+        if(this.solde>0)
+        {
+            if(montant>this.solde)
+            {
+                solde=0;
+            }
+            else
+            {
+                this.solde=this.solde-montant;
+            }
+        }
     }
 }
